546d97c9da5197d7016d9e9eb989d47a40d5b5d4
ReetVirk <85968565+ReetVirk@users.noreply.github.com>
2022-06-21 14:51:04 -0700

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -6,6 +6,6 @@ function addUser(){
     localStorage.setItem("player_1_name", player_1_var);
     localStorage.setItem("player_2_name", player_2_var);
 
-    window.location.replace("game_page.html");
+    window.location.replace("quiz_game_page.html");
 
 }
